238ecd37e2077e0160cd6b3a5c29fc417f4c1180
Stefan Szasz <paichpresident@gmail.com>
2014-04-16 15:57:47 +0300

    Fix tests

--- Dapper.Accesa/DapperTests.cs
+++ Dapper.Accesa/DapperTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using Dapper.Accesa.Model;
 using FizzWare.NBuilder;
 using Xunit;
-using Assert = Xunit.Assert;
+using Xunit.Should;
 
 namespace Dapper.Accesa
 {
@@ -22,14 +22,16 @@ namespace Dapper.Accesa
             using (var sqlConnection = new SqlConnection(ConnectionString))
             {
                 sqlConnection.Open();
-                var users = Builder<User>.CreateListOfSize(100).All().Do(user => user.Id = 0).Build();
+                var users = Builder<User>.CreateListOfSize(1000).All().Do(user => user.Id = 0).Build();
                 sqlConnection.Execute("insert into [users] (UserName, FirstName, LastName) values (@userName, @firstName, @lastName)", users);
 
                 List<User> fetchUsers = sqlConnection.Query<User>("select * from [users]").ToList();
 
+                fetchUsers.ShouldNotBeEmpty();
+
                 Assert.NotEmpty(fetchUsers);
 
-                sqlConnection.Execute("delete [users]");
+                CleanupDatabase(sqlConnection);
             }
         }
 
@@ -39,27 +41,21 @@ namespace Dapper.Accesa
             using (var sqlConnection = new SqlConnection(ConnectionString))
             {
                 sqlConnection.Open();
-                using (var tx = sqlConnection.BeginTransaction())
+                var customerId = InsertCustomerWithProjectData(sqlConnection);
+
+                const string selectMultiple = "select * from customers where Id = @customerId;" +
+                                               "select * from projects where CustomerId = @customerId;";
+
+                using (var queryMultiple = sqlConnection.QueryMultiple(selectMultiple, new { customerId }))
                 {
-                    var customerId = InsertCustomerWithProjectData(sqlConnection, tx);
+                    Customer foundCustomer = queryMultiple.Read<Customer>().First();
+                    var projects = queryMultiple.Read<Project>().ToList();
 
-                    const string selectMultiple = "select * from customers where Id = @customerId;" +
-                                                   "select * from projects where CustomerId = @customerId;";
-
-                    using (var queryMultiple = sqlConnection.QueryMultiple(selectMultiple, new { customerId }, tx))
-                    {
-                        Customer foundCustomer = queryMultiple.Read<Customer>().First();
-                        var projects = queryMultiple.Read<Project>().ToList();
-
-                        Assert.True(foundCustomer.Name == "Apple");
-                        Assert.True(projects.First().Name == ProjectName);
-                    }
-
-                    tx.Commit();
-
-                    sqlConnection.Execute("delete [projects]", transaction: tx);
-                    sqlConnection.Execute("delete [customers]", transaction: tx);
+                    foundCustomer.Name.ShouldBe("Apple");
+                    projects.First().Name.ShouldBe(ProjectName);
                 }
+
+                CleanupDatabase(sqlConnection);
             }
         }
 
@@ -69,27 +65,20 @@ namespace Dapper.Accesa
             using (var sqlConnection = new SqlConnection(ConnectionString))
             {
                 sqlConnection.Open();
-                using (var tx = sqlConnection.BeginTransaction())
+
+                var customerId = InsertCustomerWithProjectData(sqlConnection);
+
+                const string sql = "select * from Customers c join Projects p on c.Id = p.CustomerId where c.Id = @customerId";
+                var projects = sqlConnection.Query<Customer, Project, Project>(sql, (cust, prj) =>
                 {
-                    var customerId = InsertCustomerWithProjectData(sqlConnection, tx);
+                    prj.Customer = cust;
+                    return prj;
+                }, new { customerId });
 
-                    const string sql = "select * from Customers c join Projects p on c.Id = p.CustomerId where c.Id = @customerId";
-                    var projects = sqlConnection.Query<Customer, Project, Project>(sql, (cust, prj) =>
-                    {
-                        prj.Customer = cust;
-                        return prj;
-                    }, new { customerId }, tx);
+                Project first = projects.First();
+                first.Name.ShouldBe(ProjectName);
 
-                    Project first = projects.First();
-
-                    Assert.True(first.Name == ProjectName);
-                    Assert.True(first.Customer.Name == CustomerName);
-
-                    tx.Commit();
-
-                    sqlConnection.Execute("delete [projects]", transaction: tx);
-                    sqlConnection.Execute("delete [customers]", transaction: tx);
-                }
+                CleanupDatabase(sqlConnection);
             }
         }
 
@@ -110,16 +99,14 @@ namespace Dapper.Accesa
                 var fetchUsers = sqlConnection.Query("select * from [users]")
                                               .Select(row => new UserNonDefaultConstructor(row.UserName) { Id = row.Id, FirstName = row.FirstName, LastName = row.LastName })
                                               .ToList();
-                Assert.NotEmpty(fetchUsers);
-
-                sqlConnection.Execute("delete [users]");
+                fetchUsers.ShouldNotBeEmpty();
+                CleanupDatabase(sqlConnection);
             }
         }
-
-        private static int InsertCustomerWithProjectData(SqlConnection sqlConnection, SqlTransaction tx)
+        private static int InsertCustomerWithProjectData(SqlConnection sqlConnection)
         {
             var customer = new Customer { Name = CustomerName };
-            int customerId = sqlConnection.Query<int>("insert into [customers] (Name) values (@name); SELECT CAST(SCOPE_IDENTITY() as int)", customer, tx).Last();
+            int customerId = sqlConnection.Query<int>("insert into [customers] (Name) values (@name); SELECT CAST(SCOPE_IDENTITY() as int)", customer).Last();
 
             var project = new Project
             {
@@ -134,9 +121,17 @@ namespace Dapper.Accesa
             sqlConnection.Execute("insert into [projects] (Name, Description, StartDate, EndDate, Price, Type, CustomerId) " +
                                   "values " +
                                   "(@name, @description, @startDate, @endDate, @price, @type, @customerId); " +
-                                  "SELECT CAST(SCOPE_IDENTITY() as int)", project, tx);
+                                  "SELECT CAST(SCOPE_IDENTITY() as int)", project);
 
             return customerId;
         }
+
+        static void CleanupDatabase(SqlConnection sqlConnection)
+        {
+            sqlConnection.Execute("delete [projects]");
+            sqlConnection.Execute("delete [customers]");
+            sqlConnection.Execute("delete [users]");
+        }
+
     }
 }
